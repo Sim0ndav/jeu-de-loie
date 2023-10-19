b188795f901f5c0dfdd9274bfa780c6b30c5703b
Sim0ndav <si.char.david@gmail.com>
2023-10-19 11:52:17 +0200

    version-2-jeu-de-l'oie

--- src/main/java/fr/damp/App.java
+++ src/main/java/fr/damp/App.java
@@ -6,21 +6,25 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        int scoreTotal = 0;
+        int scoreTotal = 10;
         Scanner clavier = new Scanner(System.in);
-        for (int i = 1; i <= 5; i++) {
-            Random generateur = new Random();
-            int lancer = generateur.nextInt(6) + 1;
 
-            System.out.println("Votre lancer " + i + " est égal à :" + lancer);
-            scoreTotal = lancer + scoreTotal;
-            clavier.nextLine();
+        while (scoreTotal != 20) {
+            scoreTotal = 0;
+            for (int i = 1; i <= 5; i++) {
+                Random generateur = new Random();
+                int lancer = generateur.nextInt(6) + 1;
+
+                scoreTotal = lancer + scoreTotal;
+                System.out.println(
+                        "Votre lancer " + i + " est égal à :" + lancer + ".Vous êtes a la case : " + scoreTotal);
+            }
+            System.out.println("Ton score est de " + scoreTotal);
+            if (scoreTotal == 20) {
+                System.out.println("Bravo vous êtes bien arrivé a 20 !!");
+            } else
+                System.out.println("Dommage tu as perdu...");
         }
-        System.out.println("Ton score est de " + scoreTotal);
-        if (scoreTotal == 20) {
-            System.out.println("Bravo vous êtes bien arrivé a 20 !!");
-        } else
-            System.out.println("Dommage tu as perdu...");
     }
 
 }
