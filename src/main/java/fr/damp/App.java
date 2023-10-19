4ec1faf9512c28a5fdf6032200dd986b6b26f009
Sim0ndav <si.char.david@gmail.com>
2023-10-19 12:01:41 +0200

    v3-affiche le nb de partie

--- src/main/java/fr/damp/App.java
+++ src/main/java/fr/damp/App.java
@@ -8,7 +8,7 @@ public class App {
     public static void main(String[] args) {
         int scoreTotal = 10;
         Scanner clavier = new Scanner(System.in);
-
+        int nbPartie = 0;
         while (scoreTotal != 20) {
             scoreTotal = 0;
             for (int i = 1; i <= 5; i++) {
@@ -18,13 +18,16 @@ public class App {
                 scoreTotal = lancer + scoreTotal;
                 System.out.println(
                         "Votre lancer " + i + " est égal à :" + lancer + ".Vous êtes a la case : " + scoreTotal);
+
             }
             System.out.println("Ton score est de " + scoreTotal);
             if (scoreTotal == 20) {
                 System.out.println("Bravo vous êtes bien arrivé a 20 !!");
             } else
                 System.out.println("Dommage tu as perdu...");
+            nbPartie = nbPartie + 1;
         }
+        System.out.println("vous avez fait " + nbPartie + " partie.");
     }
 
 }
