0ed156736d470ef694342272588360d184251ef9
Sim0ndav <si.char.david@gmail.com>
2023-11-16 11:35:46 +0100

    autre version jeu de l'oie

--- src/main/java/fr/damp/App.java
+++ src/main/java/fr/damp/App.java
@@ -7,29 +7,47 @@ public class App {
 
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
+        int Objectif = 20;
+        int caseCourante = 0;
+        int nbLancers = 0;
 
-        int scoreTotal = 0;
-        int nbPartie = 0;
+        while (caseCourante != 20) {
 
-        while (scoreTotal != 20) {
-            scoreTotal = 0;
-            for (int i = 1; i <= 5; i++) {
-                Random generateur = new Random();
-                int lancer = generateur.nextInt(6) + 1;
+            Random generateur = new Random();
+            int lancer = generateur.nextInt(6) + 1;
+            nbLancers++;
+            caseCourante = lancer + caseCourante;
 
-                scoreTotal = lancer + scoreTotal;
-                System.out.println(
-                        "Votre lancer " + i + " est égal à :" + lancer + ".Vous êtes a la case : " + scoreTotal);
+            if (caseCourante > Objectif) {
+                int depassement = caseCourante - Objectif;
+                caseCourante = Objectif - depassement;
 
             }
-            System.out.println("Ton score est de " + scoreTotal);
-            if (scoreTotal == 20) {
-                System.out.println("Bravo vous êtes bien arrivé a 20 !!");
-            } else
-                System.out.println("Dommage tu as perdu...");
-            nbPartie = nbPartie + 1;
+            System.err.println("votre lancer " + nbLancers + " vous mène a la case " + caseCourante);
         }
-        System.out.println("vous avez fait " + nbPartie + " partie.");
+        System.err.println("Il vous aura fallu " + nbLancers + " lancer pour gagner");
+        if (caseCourante == Objectif) {
+            System.err.println("Vous avez gagné !!!");
+        }
+
+        // for (int i = 1; i <= 5; i++) {
+        // Random generateur = new Random();
+        // int lancer = generateur.nextInt(6) + 1;
+
+        // scoreTotal = lancer + scoreTotal;
+        // System.out.println(
+        // "Votre lancer " + i + " est égal à :" + lancer + ".Vous êtes a la case : " +
+        // scoreTotal);
+
+        // }
+        // System.out.println("Ton score est de " + scoreTotal);
+        // if (scoreTotal == 20) {
+        // System.out.println("Bravo vous êtes bien arrivé a 20 !!");
+        // } else
+        // System.out.println("Dommage tu as perdu...");
+        // nbPartie = nbPartie + 1;
+        // }
+        // System.out.println("vous avez fait " + nbPartie + " partie.");
     }
 
 }
